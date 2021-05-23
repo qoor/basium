qoor/basium

commit a8ad753c14ae130d5892a5a9c126956ee28a9e3b

DCScan: Implement DCScan based on userfaultfd
This is a version of DCScan based on userfaultfd and existing safepoint
infrastructure. The basic algorithm is:
1) as soon as the scanner starts scanning a slot span, it protects it
   entirely;
2) when mutator hits the fault, the handler is called. This handler
   synchronously enters the safepoint. Other mutators hitting the fault
   will be blocked waiting for the handler to finish scanning;
3) at the end of scanning all slot spans are unprotected again.

Bug: 1206047
Change-Id: I7f116d4cbdbd69ad9cd201aaa19ae620d4ed722d
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875430
Commit-Queue: Anton Bikineev <bikineev@chromium.org>
Auto-Submit: Anton Bikineev <bikineev@chromium.org>
Reviewed-by: Will Harris <wfh@chromium.org>
Reviewed-by: Kentaro Hara <haraken@chromium.org>
Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
Owners-Override: Kentaro Hara <haraken@chromium.org>
Cr-Commit-Position: refs/heads/master@{#884767}

base/allocator/partition_allocator/starscan/pcscan_internal.cc
@@ -22,6 +22,7 @@
 #include "base/allocator/partition_allocator/partition_address_space.h"
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "base/allocator/partition_allocator/partition_alloc_check.h"
+#include "base/allocator/partition_allocator/partition_alloc_config.h"
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
 #include "base/allocator/partition_allocator/partition_alloc_features.h"
 #include "base/allocator/partition_allocator/partition_page.h"
@@ -303,14 +304,11 @@ void PCScanSnapshot::Take(size_t pcscan_epoch) {
     typename Root::ScopedGuard guard(root->lock_);
 
     // Take a snapshot of all super pages and scannable slot spans.
-    // TODO(bikineev): Consider making current_extent lock-free and moving it
-    // to the concurrent thread.
     for (auto* super_page_extent = root->first_extent; super_page_extent;
          super_page_extent = super_page_extent->next) {
       for (char* super_page = super_page_extent->super_page_base;
            super_page != super_page_extent->super_pages_end;
            super_page += kSuperPageSize) {
-        // TODO(bikineev): Consider following freelists instead of slot spans.
         const size_t visited_slot_spans = IterateSlotSpans<ThreadSafe>(
             super_page, true /*with_quarantine*/,
             [this](SlotSpan* slot_span) -> bool {
@@ -424,11 +422,13 @@ class PCScanTask final : public base::RefCountedThreadSafe<PCScanTask>,
       }
     }
     ~SyncScope() {
-      // First, notify other scanning threads that this thread is done.
+      // First, notify the scanning thread that this thread is done.
       NotifyThreads();
+      // Then, unprotect all scanned pages, if needed.
+      task_.UnprotectPartitions();
       if (context == Context::kScanner) {
         // The scanner thread must wait here until all safepoints leave.
-        // Otherwise, sweeping may free a page that can be accessed by a
+        // Otherwise, sweeping may free a page that can later be accessed by a
         // descheduled mutator.
         WaitForOtherThreads();
       }
@@ -491,6 +491,9 @@ class PCScanTask final : public base::RefCountedThreadSafe<PCScanTask>,
   // Clear quarantined objects and prepare card table for fast lookup
   void ClearQuarantinedObjectsAndPrepareCardTable();
 
+  // Unprotect all slot spans from all partitions.
+  void UnprotectPartitions();
+
   // Sweeps (frees) unreachable quarantined entries. Returns the size of swept
   // objects.
   void SweepQuarantine();
@@ -507,6 +510,8 @@ class PCScanTask final : public base::RefCountedThreadSafe<PCScanTask>,
   std::mutex mutex_;
   std::condition_variable condvar_;
   std::atomic<size_t> number_of_scanning_threads_{0u};
+  // We can unprotect only once to reduce context-switches.
+  std::once_flag unprotect_once_flag_;
   PCScan& pcscan_;
 };
 
@@ -608,6 +613,19 @@ void PCScanTask::ClearQuarantinedObjectsAndPrepareCardTable() {
   });
 }
 
+void PCScanTask::UnprotectPartitions() {
+  std::call_once(unprotect_once_flag_, [this] {
+    auto& pcscan = PCScanInternal::Instance();
+    const auto unprotect = [&pcscan](const auto& slot_span) {
+      pcscan.UnprotectPages(
+          reinterpret_cast<uintptr_t>(slot_span.begin),
+          (slot_span.end - slot_span.begin) * sizeof(uintptr_t));
+    };
+    snapshot_.large_scan_areas_worklist().VisitNonConcurrently(unprotect);
+    snapshot_.scan_areas_worklist().VisitNonConcurrently(unprotect);
+  });
+}
+
 class PCScanScanLoop final : public ScanLoop<PCScanScanLoop> {
   friend class ScanLoop<PCScanScanLoop>;
 
@@ -696,7 +714,11 @@ void PCScanTask::ScanPartitions() {
   // is scanned contains quarantined objects.
   PCScanSnapshot::LargeScanAreasWorklist::RandomizedView large_scan_areas(
       snapshot_.large_scan_areas_worklist());
-  large_scan_areas.Visit([this, &scan_loop](auto scan_area) {
+  auto& pcscan = PCScanInternal::Instance();
+  large_scan_areas.Visit([this, &scan_loop, &pcscan](auto scan_area) {
+    // Protect slot span before scanning it.
+    pcscan.ProtectPages(reinterpret_cast<uintptr_t>(scan_area.begin),
+                        (scan_area.end - scan_area.begin) * sizeof(uintptr_t));
     // The bitmap is (a) always guaranteed to exist and (b) the same for all
     // objects in a given slot span.
     // TODO(chromium:1129751): Check mutator bitmap as well if performance
@@ -722,7 +744,10 @@ void PCScanTask::ScanPartitions() {
   // Scan areas with regular size slots.
   PCScanSnapshot::ScanAreasWorklist::RandomizedView scan_areas(
       snapshot_.scan_areas_worklist());
-  scan_areas.Visit([&scan_loop](auto scan_area) {
+  scan_areas.Visit([&scan_loop, &pcscan](auto scan_area) {
+    // Protect slot span before scanning it.
+    pcscan.ProtectPages(reinterpret_cast<uintptr_t>(scan_area.begin),
+                        (scan_area.end - scan_area.begin) * sizeof(uintptr_t));
     scan_loop.Run(scan_area.begin, scan_area.end);
   });
 
@@ -970,10 +995,18 @@ PCScanInternal::PCScanInternal() : simd_support_(DetectSimdSupport()) {}
 
 PCScanInternal::~PCScanInternal() = default;
 
-void PCScanInternal::Initialize() {
+void PCScanInternal::Initialize(PCScan::WantedWriteProtectionMode wpmode) {
   PA_DCHECK(!is_initialized_);
   CommitCardTable();
-  PCScan::SetClearType(PCScan::ClearType::kLazy);
+#if defined(PA_STARSCAN_UFFD_WRITE_PROTECTOR_SUPPORTED)
+  if (wpmode == PCScan::WantedWriteProtectionMode::kEnabled)
+    write_protector_ = std::make_unique<UserFaultFDWriteProtector>();
+  else
+    write_protector_ = std::make_unique<NoWriteProtector>();
+#else
+  write_protector_ = std::make_unique<NoWriteProtector>();
+#endif  // defined(PA_STARSCAN_UFFD_WRITE_PROTECTOR_SUPPORTED)
+  PCScan::SetClearType(write_protector_->SupportedClearType());
   is_initialized_ = true;
 }
 
@@ -1137,6 +1170,23 @@ void* PCScanInternal::GetCurrentThreadStackTop() const {
   return it != stack_tops_.end() ? it->second : nullptr;
 }
 
+void PCScanInternal::ProtectPages(uintptr_t begin, size_t size) {
+  // Slot-span sizes are multiple of system page size. However, the ranges that
+  // are recorded are not, since in the snapshot we only record the used
+  // payload. Therefore we align up the incoming range by 4k. The unused part of
+  // slot-spans doesn't need to be protected (the allocator will enter the
+  // safepoint before trying to allocate from it).
+  PA_DCHECK(write_protector_.get());
+  write_protector_->ProtectPages(
+      begin, (size + SystemPageSize() - 1) & ~(SystemPageSize() - 1));
+}
+
+void PCScanInternal::UnprotectPages(uintptr_t begin, size_t size) {
+  PA_DCHECK(write_protector_.get());
+  write_protector_->UnprotectPages(
+      begin, (size + SystemPageSize() - 1) & ~(SystemPageSize() - 1));
+}
+
 void PCScanInternal::ClearRootsForTesting() {
   // Set all roots as non-scannable and non-quarantinable.
   for (auto* root : scannable_roots_) {
@@ -1150,9 +1200,9 @@ void PCScanInternal::ClearRootsForTesting() {
   nonscannable_roots_.ClearForTesting();  // IN-TEST
 }
 
-void PCScanInternal::ReinitForTesting() {
+void PCScanInternal::ReinitForTesting(PCScan::WantedWriteProtectionMode mode) {
   is_initialized_ = false;
-  Initialize();
+  Initialize(mode);
 }
 
 void PCScanInternal::FinishScanForTesting() {

base/BUILD.gn
@@ -1937,6 +1937,8 @@ component("base") {
         "allocator/partition_allocator/starscan/starscan_fwd.h",
         "allocator/partition_allocator/starscan/stats_collector.cc",
         "allocator/partition_allocator/starscan/stats_collector.h",
+        "allocator/partition_allocator/starscan/write_protector.cc",
+        "allocator/partition_allocator/starscan/write_protector.h",
         "allocator/partition_allocator/thread_cache.cc",
         "allocator/partition_allocator/thread_cache.h",
         "allocator/partition_allocator/yield_processor.h",

base/allocator/allocator_shim.h
@@ -167,7 +167,7 @@ BASE_EXPORT void ConfigurePartitionRefCountSupport(bool enable_ref_count);
 #endif
 
 #if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC) && PA_ALLOW_PCSCAN
-BASE_EXPORT void EnablePCScan();
+BASE_EXPORT void EnablePCScan(bool dcscan);
 #endif
 
 }  // namespace allocator

base/allocator/allocator_shim_default_dispatch_to_partition_alloc.cc
@@ -484,8 +484,10 @@ void ConfigurePartitionRefCountSupport(bool enable_ref_count) {
 #endif  // BUILDFLAG(ENABLE_RUNTIME_BACKUP_REF_PTR_CONTROL)
 
 #if PA_ALLOW_PCSCAN
-void EnablePCScan() {
-  internal::PCScan::Initialize();
+void EnablePCScan(bool dcscan) {
+  internal::PCScan::Initialize(
+      dcscan ? internal::PCScan::WantedWriteProtectionMode::kEnabled
+             : internal::PCScan::WantedWriteProtectionMode::kDisabled);
   internal::PCScan::RegisterScannableRoot(Allocator());
   if (Allocator() != AlignedAllocator())
     internal::PCScan::RegisterScannableRoot(AlignedAllocator());

base/allocator/partition_allocator/partition_alloc_config.h
@@ -34,6 +34,12 @@ static_assert(sizeof(void*) != 8, "");
 #define PA_STARSCAN_NEON_SUPPORTED
 #endif
 
+#if defined(PA_HAS_64_BITS_POINTERS) && \
+    (defined(OS_LINUX) || defined(OS_ANDROID))
+// TODO(bikineev): Enable for ChromeOS.
+#define PA_STARSCAN_UFFD_WRITE_PROTECTOR_SUPPORTED
+#endif
+
 // POSIX is not only UNIX, e.g. macOS and other OSes. We do use Linux-specific
 // features such as futex(2).
 #if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID)

base/allocator/partition_allocator/partition_alloc_features.cc
@@ -54,5 +54,8 @@ const Feature kPartitionAllocPCScanStackScanning {
 #endif  // defined(PA_PCSCAN_STACK_SUPPORTED)
 };
 
+const Feature kPartitionAllocDCScan{"PartitionAllocDCScan",
+                                    FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace features
 }  // namespace base

base/allocator/partition_allocator/partition_alloc_features.h
@@ -28,6 +28,7 @@ extern const BASE_EXPORT Feature kPartitionAllocLargeThreadCacheSize;
 
 extern const BASE_EXPORT Feature kPartitionAllocPCScanMUAwareScheduler;
 extern const BASE_EXPORT Feature kPartitionAllocPCScanStackScanning;
+extern const BASE_EXPORT Feature kPartitionAllocDCScan;
 
 extern const BASE_EXPORT Feature kPartitionAllocLazyCommit;
 

base/allocator/partition_allocator/starscan/pcscan.cc
@@ -9,8 +9,8 @@
 namespace base {
 namespace internal {
 
-void PCScan::Initialize() {
-  PCScanInternal::Instance().Initialize();
+void PCScan::Initialize(WantedWriteProtectionMode wpmode) {
+  PCScanInternal::Instance().Initialize(wpmode);
 }
 
 void PCScan::RegisterScannableRoot(Root* root) {
@@ -68,8 +68,8 @@ void PCScan::UninitForTesting() {
   ReinitPCScanMetadataAllocatorForTesting();          // IN-TEST
 }
 
-void PCScan::ReinitForTesting() {
-  PCScanInternal::Instance().ReinitForTesting();  // IN-TEST
+void PCScan::ReinitForTesting(WantedWriteProtectionMode wpmode) {
+  PCScanInternal::Instance().ReinitForTesting(wpmode);  // IN-TEST
 }
 
 void PCScan::FinishScanForTesting() {

commit acf41af5be151af3cb7eb7e8924fd6142400e22b

*Scan: Fix test failures caused by double free in write_protector
a8ad753c14ae1 introduced write_protector unique_ptr into PCScanInternal.
In tests there was a scenario:
1) upon PCScan test destruction PartitionAllocGlobalUninitForTesting()
is called;
2) the new PCScan test calls PCScanInternal::Initialize(), which resets
write_protector, trying to delete existing pointer, which was already
deleted by PartitionAllocGlobalUninitForTesting().

The CL resets this unique_ptr explicitly in
PCScanInternal::ClearRootsForTesting().

Bug: 1206047
Change-Id: I795b0ff84ecfb90a10735c60432caf02c9f7d54e
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2913257
Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
Auto-Submit: Anton Bikineev <bikineev@chromium.org>
Cr-Commit-Position: refs/heads/master@{#885822}

base/allocator/partition_allocator/starscan/pcscan_internal.cc
@@ -1198,6 +1198,9 @@ void PCScanInternal::ClearRootsForTesting() {
   }
   scannable_roots_.ClearForTesting();     // IN-TEST
   nonscannable_roots_.ClearForTesting();  // IN-TEST
+  // Destroy write protector object, so that there is no double free on the next
+  // call to ReinitForTesting();
+  write_protector_.reset();
 }
 
 void PCScanInternal::ReinitForTesting(PCScan::WantedWriteProtectionMode mode) {