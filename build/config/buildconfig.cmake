33208844c2f05023106855d5f175a3daf72268b4
MaBlingm <akck0918@gmail.com>
2022-03-24 22:53:31 +0900

    Add some basic CMake modules for cross-compile

---
+++ build/config/buildconfig.cmake
@@ -0,0 +1,13 @@
+set(CHROMEOS OFF)
+set(LINUX OFF)
+set(MAC OFF)
+
+if(NOT ANDROID AND NOT IOS)
+  if(CMAKE_SYSTEM_NAME STREQUAL "chromeos")
+    set(CHROMEOS ON)
+  elseif(CMAKE_SYSTEM_TYPE STREQUAL "Linux")
+    set(LINUX ON)
+  elseif(APPLE)
+    set(MAC ON)
+  endif()
+endif()
